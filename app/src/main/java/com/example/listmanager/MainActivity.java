f6e4ace210749950dacf7637bbf048ad87660803
Andy <ADG82@pitt.edu>
2020-02-12 14:45:13 -0500

    Added functionality to Enter Screen buttons
    Enter sends user to Main list activity and exit leaves the application

--- app/src/main/java/com/example/listmanager/MainActivity.java
+++ app/src/main/java/com/example/listmanager/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.listmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Will take the user to the main page
+     * in which will contain a list view of the users lists
+     * THis method will also connect the app to the local db
+     * @param view
+     */
+    public void EnterApp(View view){
+        Intent intent = new Intent(MainActivity.this, MainListActivity.class);
+        startActivity(intent);
+    }
+
+    //Will just exit the app
+    public void ExitApp(View view){
+        finish();
+        System.exit(0);
     }
 }
